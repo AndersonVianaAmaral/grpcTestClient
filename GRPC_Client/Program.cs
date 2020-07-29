19d98f5bb1b43a170b693b4e46f4b6a9e35c6128
Amaral, Anderson <anderson.amaral@pearson.com>
2020-07-29 14:40:57 -0300

    this project

---
+++ GRPC_Client/Program.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Net.Client;
+using GrpcService;
+using System;
+using System.Threading.Tasks;
+
+namespace GRPC_Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var i = 0;
+            while (i <= 10000)
+            {
+                var client = new Greeter.GreeterClient(GrpcChannel.ForAddress("https://localhost:5001"));
+                var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+                Console.WriteLine("Greeting: " + reply.Message +"    " + i);
+                i++;
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
